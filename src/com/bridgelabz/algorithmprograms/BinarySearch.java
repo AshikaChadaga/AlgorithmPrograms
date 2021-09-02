16a88319c29a09565e00f6424d152921aaee6c5b
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-02 15:47:10 +0530

    REFACTORED: Implemented Using Java Generics

--- src/com/bridgelabz/algorithmprograms/BinarySearch.java
+++ src/com/bridgelabz/algorithmprograms/BinarySearch.java
@@ -3,42 +3,46 @@ package com.bridgelabz.algorithmprograms;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class BinarySearch {
-
-	public int binarySearch(ArrayList<String> wordArray, int low, int high, String key) {
+public class BinarySearch<T extends Comparable<T>> {
+	
+	public int binarySearch(ArrayList<T> arrayToBeSearched, int low, int high, T key) {
 		if (high >= low) {
 			int middle = low + (high - low) / 2;
-			if (wordArray.get(middle).equals(key)) {
+			if (arrayToBeSearched.get(middle).equals(key)) {
 				return middle;
 			}
-			if (wordArray.get(middle).compareTo(key) > 0) {
-				return binarySearch(wordArray, low, middle - 1, key);
+			if (arrayToBeSearched.get(middle).compareTo(key) > 0) {
+				return binarySearch(arrayToBeSearched, low, middle - 1, key);
 			}
-			return binarySearch(wordArray, middle + 1, high, key);
+			return binarySearch(arrayToBeSearched, middle + 1, high, key);
 		}
 		return -1;
 	}
 
 	public static void main(String[] args) {
 
-		BinarySearch searchObject = new BinarySearch();
+		BinarySearch<Integer> searchObject = new BinarySearch<Integer>();
 		Scanner scannerObject = new Scanner(System.in);
-		ArrayList<String> wordArray = new ArrayList<String>();
-		System.out.println("Enter the number of words: ");
+		ArrayList<Integer> arrayToBeSearched = new ArrayList<Integer>();
+		
+		System.out.println("Enter the size of The Array: ");
 		int sizeOfArray = scannerObject.nextInt();
-		System.out.println("Enter words");
+		
+		System.out.println("Enter Array Elements in Sorted Order");
 		for (int index = 0; index < sizeOfArray; index++) {
-			String word = scannerObject.next();
-			wordArray.add(word);
+			Integer word = scannerObject.nextInt();
+			arrayToBeSearched.add(word);
 		}
 
-		System.out.println("Enter the word to be searched");
-		String wordToBeSearched = scannerObject.next();
+		System.out.println("Enter the element to be searched");
+		Integer ElementToBeSearched = scannerObject.nextInt();
 		scannerObject.close();
-		System.out.println("The array of words are : " + wordArray);
-		int indexOfWord = searchObject.binarySearch(wordArray, 0, sizeOfArray - 1, wordToBeSearched);
-		System.out.println("Word [" + wordToBeSearched + "] found at index: " + indexOfWord);
+		
+		System.out.println("The array is : " + arrayToBeSearched);
+		int indexOfWord = searchObject.binarySearch(arrayToBeSearched, 0, sizeOfArray-1, ElementToBeSearched);
+		System.out.println("Element [" + ElementToBeSearched + "] found at index: " + indexOfWord);
 
 	}
+
 
 }
